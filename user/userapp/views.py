f8fbad216f34dbc18d37c180089abac3dc068cda
ROHIT KUMAR YADAV <109666751+Rohityadav2205@users.noreply.github.com>
2023-04-08 18:32:09 +0530

    using json and postman

--- user/userapp/views.py
+++ user/userapp/views.py
@@ -1,71 +1,94 @@
-from django.shortcuts import render
+from django.core import serializers
+from django.shortcuts import HttpResponse,render
+from django.http import JsonResponse
+
 
 from .models import userModel
 
 
 # Create your views here.
-# def rohit(request):
-#     return HttpResponse("hello Ram")
 
 def create(request):
-    user_name = 0
-    user_email = 0
-    mobile_number = 0
+    user_name = request.GET["user_name"]
+    user_email = request.GET["user_email"]
+    mobile_number = request.GET["mobile_number"]
+    print("mobile number", mobile_number)
 
-    submit = 0
-    if request.GET:
-        user_name = request.GET["user_name"]
-        user_email = request.GET["user_email"]
-        mobile_number = request.GET["mobile_number"]
-        print("mobile number", mobile_number)
+    ms = userModel()
+    ms.user_name = user_name
+    ms.user_email = user_email
+    ms.mobile_number = mobile_number
 
-        opt = request.GET["option"]
-        ms = userModel()
-        ms.user_name = user_name
-        ms.user_email = user_email
-        ms.mobile_number = mobile_number
+    ms.save()
 
-        ms.save()
-        print("saved")
-
-        if opt == "submit":
-            submit = user_name
-
-    return render(request, "create.html",
-                  {"user_name": user_name, "user_email": user_email, "mobile_number": mobile_number, "submit": submit})
+    output = serializers.serialize("json", [ms])
+    return HttpResponse(output, content_type="application/json")
 
 
+def jsonall(request):
+    output = serializers.serialize("json", userModel.objects.all())
+    return HttpResponse(output, content_type="application/json")
+
+
+# show database record on the server page
 def retrieve(request):
     users = userModel.objects.all()
-    return render(request, "retrieve.html", {'users': users})
+    output = serializers.serialize("json", userModel.objects.all())
+    return HttpResponse(output, content_type="application/json")
 
 
 def edit(request):
     id = request.GET["id"]
-    users = userModel.objects.get(id=id)
-    return render(request, 'edit.html', {'users': users})
+    try:
+        users = userModel.objects.get(id=id)
+        print(users)
+        users.user_name="shubham";
+        users.user_email="subh@gmail.com";
+        users.mobile_number="8899900556";
+        users.save()
+        users = [users]
+    except:
+        return HttpResponse("No data")
+    output = serializers.serialize("json", users)
+    return HttpResponse(output, content_type="application/json")
 
 
 def update(request):
     id = request.GET["id"]
     users = userModel.objects.get(id=id)
-    if request.POST:
-        user_name = request.POST["user_name"]
-        user_email=request.POST["user_email"]
-        mobile_number=request.POST["mobile_number"]
-        users.user_name = user_name
-        users.user_email =user_email
-        users.mobile_number=mobile_number
-        users.save()
+    try:
 
-    return render(request, 'edit.html', {'users': users})
+        if request.POST:
+
+            user_name = request.POST["user_name"]
+            user_email = request.POST["user_email"]
+            mobile_number = request.POST["mobile_number"]
+            users.user_name = user_name
+            users.user_email = user_email
+            users.mobile_number = mobile_number
+
+
+            users.save()
+            print(users)
+            users = [users]
+
+    except:
+        error ={"status":"failed"}
+        return HttpResponse("data update")
+
+    output = serializers.serialize("json", users)
+    return HttpResponse(output, content_type="application/json")
 
 
 def delete(request):
-    if request.GET:
-        id = request.GET["id"]
-        users = userModel.objects.filter(id=id)[0]
-        users.delete()
-
-    return render(request, "delete.html", {"users": users})
-#
+    try:
+        if request.GET:
+            id = request.GET["id"]
+            users = userModel.objects.filter(id=id)[0]
+            users.delete()
+            print(users)
+            users = [users]
+            output = serializers.serialize("json", users)
+            return HttpResponse(output, content_type="application/json")
+    except:
+        return HttpResponse("no Data")
